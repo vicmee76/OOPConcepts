9a1b1fc0b41eb130f743fda54855dab5f857017f
ComputerGuy <vicmee76@hotmail.com>
2022-04-18 03:18:32 +0100

    -- implemented abstractions

---
+++ Abstractions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPConcepts
+{
+    public class Abstractions
+    {
+        public abstract class AbstractCars
+        {
+            public abstract int SeatNumber();
+
+            public virtual string CarColor()
+            {
+                return "Car color is black";
+            }
+        }
+
+
+        public class Toyota : AbstractCars
+        {
+            public override int SeatNumber()
+            {
+                return 4;
+            }
+
+            public override string CarColor()
+            {
+                return "Car color is now red";
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static OOPConcepts.Abstractions;
 using static OOPConcepts.Inheritance;
 using static OOPConcepts.Polymorphism;
 
@@ -8,29 +9,39 @@ namespace OOPConcepts
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Inheritance");
             Father father = new Father();
             father.FatherHairColor();
             Son son = new Son();
             son.FatherHairColor();
             son.SonHairColor();
 
-            Console.WriteLine("");
-
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Encapsulation");
             Encapsulation enp = new Encapsulation();
             enp.sayName();
             Console.WriteLine(enp.sayAge);
             Console.WriteLine(enp.sayGender);
 
 
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Polymorphism - Method Overloading");
             MethodOverLoading load = new MethodOverLoading();
             Console.WriteLine(load.AddNums(908, 6876));
             Console.WriteLine(load.AddNums(5.2, 6.5));
             Console.WriteLine(load.AddNums("My", "Name", "Is"));
-            load.SayMyName();
 
-            MethodOverridding over = new MethodOverridding();
+            Console.WriteLine("Polymorphism - Method Overidding");
+            MethodOverridding over = new MethodOverridding(); 
+            load.SayMyName();
             over.SayMyName();
 
+
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Abstraction");
+            Toyota t = new Toyota();
+            Console.WriteLine(t.CarColor());
+            Console.WriteLine(t.SeatNumber());
 
         }
     }
